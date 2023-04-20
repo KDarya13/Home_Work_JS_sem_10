0c7cddd19769a0e0c0e631717f447e6d9f25f100
Darya <Darsy_13@bk.ru>
2023-04-20 18:17:32 +0300

    data

---
+++ data.js
@@ -0,0 +1,20 @@
+export const dataColorInfo = `[
+    {
+    "id": "1",
+    "color": "coral",
+    "number": "008000",
+    "url": "https://cdn.shopify.com/s/files/1/0068/2292/0307/products/il_fullxfull.760165467_rwi4_300x.jpg?v=1577285768"
+},
+{
+    "id": "2",
+    "color": "turquoise",
+    "number": "30d5c8",
+    "url": "https://i.pinimg.com/236x/58/04/38/580438f0ee6659fab4e4a5a3cf8aa2f8.jpg"
+},
+{
+    "id": "3",
+    "color": "sand",
+    "number": "fcdd76",
+    "url": "https://img-10.wfolio.com/5EKvcyg-f2CO8Dt4YezkAiHJsUtbSLIDOwgTfLZG5Qs/c:400:400:nowe:0:0/rs:fill:320:0:0/cb:v2/aHR0cDovL3N0b3Jh/Z2Uud2ZvbGlvLnJ1/L3NpdGVzLzE2Ni9h/c3NldHMvMTUyNDk0/MTA4MF83NmUwOWQu/anBn.jpg"
+}
+]`;
